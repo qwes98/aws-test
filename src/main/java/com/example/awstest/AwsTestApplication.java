0b05d9aab34751be0eead2cb00edea1d42999726
Jiwon Park <qwes8873@gmail.com>
2023-05-11 20:58:01 +0900

    Add pid

--- src/main/java/com/example/awstest/AwsTestApplication.java
+++ src/main/java/com/example/awstest/AwsTestApplication.java
@@ -2,12 +2,15 @@ package com.example.awstest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.ApplicationPidFileWriter;
 
 @SpringBootApplication
 public class AwsTestApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AwsTestApplication.class, args);
+		SpringApplication application = new SpringApplication(AwsTestApplication.class);
+		application.addListeners(new ApplicationPidFileWriter());
+		application.run(args);
 	}
 
 }
